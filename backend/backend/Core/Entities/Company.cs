0310c4f3b8a56fbdb5bd0c61145a0d38f33d292f
Ilya Konovalov <broken_67@bk.ru>
2024-06-26 00:35:28 +0300

    ver - 0.0 - Структура базы данных - Реализация ApplicationDbContext

---
+++ backend/backend/Core/Entities/Company.cs
@@ -0,0 +1,13 @@
+﻿using backend.Core.Enums;
+
+namespace backend.Core.Entities
+{
+    public class Company : BaseEntity
+    {
+        public string Name { get; set; }
+        public CompanySize Size { get; set; }
+
+        //Связи
+        public ICollection<Job> Jobs { get; set; }
+    }
+}
---
+++ backend/backend/Core/Entities/Job.cs
@@ -0,0 +1,15 @@
+﻿using backend.Core.Enums;
+
+namespace backend.Core.Entities
+{
+    public class Job : BaseEntity
+    {
+        public string Title { get; set; }
+        public JobLevel Level { get; set; }
+        //Связи
+        public long CompanyId { get; set; } 
+        public Company Company { get; set; }
+        public ICollection<Candidate> Candidates { get;set; }
+
+    }
+}
